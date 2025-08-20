fc5f1d362bb8a9c0e5f2f60c810b14ed2a05b64b
SelcukKesapli <kesapliselcuk@gmail.com>
2025-08-20 19:29:00 +0300

    logs eklendi

--- src/test/java/stepDefinitions/TumHizmetlerPageSteps.java
+++ src/test/java/stepDefinitions/TumHizmetlerPageSteps.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import logs.Log;
+import logpackages.Log;
 import org.junit.Assert;
 import pages.*;
 import utilities.ConfigReader;
