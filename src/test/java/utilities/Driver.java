cad616b41fafdf6cb847d627fad04965767b36c9
SelcukKesapli <kesapliselcuk@gmail.com>
2025-08-23 12:27:24 +0300

    Driver ve pom.xml guncellendi

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,7 +1,11 @@
 package utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Driver {
 
@@ -9,14 +13,40 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            String browser = System.getProperty("browser", "chrome");
+            boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+
+            switch (browser.toLowerCase()) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    if (headless) {
+                        firefoxOptions.addArguments("--headless");
+                    }
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
+                case "chrome":
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (headless) {
+                        chromeOptions.addArguments("--headless");
+                        chromeOptions.addArguments("--no-sandbox");
+                        chromeOptions.addArguments("--disable-dev-shm-usage");
+                    }
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+            }
+
             driver.manage().window().maximize();
         }
         return driver;
     }
 
     public static void closeDriver() {
-        driver.quit();
-        driver = null;
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
